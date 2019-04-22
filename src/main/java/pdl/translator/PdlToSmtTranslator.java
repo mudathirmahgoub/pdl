a263f1ef600ac43f994cf0f34841239961a9ba0c
mudathirmahgoub <mudathirmahgoub@gmail.com>
2019-04-21 21:22:17 -0500

    refactoring

--- src/main/java/pdl/translator/PdlToSmtTranslator.java
+++ src/main/java/pdl/translator/PdlToSmtTranslator.java
@@ -2,6 +2,7 @@ package pdl.translator;
 
 import edu.uiowa.smt.AbstractTranslator;
 import edu.uiowa.smt.smtAst.SmtProgram;
+import pdl.ast.KripkeFrame;
 import pdl.ast.PdlProgram;
 
 public class PdlToSmtTranslator extends AbstractTranslator
@@ -15,6 +16,20 @@ public class PdlToSmtTranslator extends AbstractTranslator
     @Override
     public SmtProgram translate()
     {
+        translateFrame();
+        return translateFormula();
+    }
+
+    private SmtProgram translateFormula()
+    {
         throw new UnsupportedOperationException();
+    }
+
+    private void translateFrame()
+    {
+        if(program.isFrameProvided())
+        {
+            throw new UnsupportedOperationException();
+        }
     }
 }
