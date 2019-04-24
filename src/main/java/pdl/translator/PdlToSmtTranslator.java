c80f5c3d2ac0bd413cf278b3e069774aef762d5e
mudathirmahgoub <mudathirmahgoub@gmail.com>
2019-04-23 23:52:28 -0500

    kripke frame

--- src/main/java/pdl/translator/PdlToSmtTranslator.java
+++ src/main/java/pdl/translator/PdlToSmtTranslator.java
@@ -9,6 +9,7 @@ import pdl.ast.Transition;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PdlToSmtTranslator extends AbstractTranslator
 {
@@ -19,6 +20,7 @@ public class PdlToSmtTranslator extends AbstractTranslator
     public Expression statesIdentity;
     public Map<String, FunctionDeclaration> propositionMap;
     public Map<String, FunctionDeclaration> programsMap;
+    public Map<String, FunctionDeclaration> statesMap;
 
     PdlToSmtTranslator(PdlProgram program)
     {
@@ -26,7 +28,8 @@ public class PdlToSmtTranslator extends AbstractTranslator
         this.smtProgram = new SmtProgram();
         this.propositionMap = new HashMap<>();
         this.programsMap = new HashMap<>();
-        statesUniverse =  atomUniverse.getVariable();
+        statesMap = new HashMap<>();
+        statesUniverse = atomUniverse.getVariable();
         statesIdentity = atomIdentity.getVariable();
 
         translateSpecialFunctions();
@@ -86,9 +89,9 @@ public class PdlToSmtTranslator extends AbstractTranslator
     private void translateFrame()
     {
         KripkeFrame frame = pdlProgram.getFrame();
-        for (String state: frame.getStates())
+        if (pdlProgram.isFrameProvided())
         {
-            throw new UnsupportedOperationException();
+            translateFrameStates(frame);
         }
 
         for (Map.Entry<String, List<String>> entry : frame.getPropositions().entrySet())
@@ -97,9 +100,9 @@ public class PdlToSmtTranslator extends AbstractTranslator
             FunctionDeclaration declaration = new FunctionDeclaration(name, AbstractTranslator.setOfUnaryAtomSort);
             this.propositionMap.put(name, declaration);
             this.smtProgram.addFunction(declaration);
-            for (String state: entry.getValue())
+            if (pdlProgram.isFrameProvided())
             {
-                throw new UnsupportedOperationException();
+                translatePropositionStates(entry.getValue(), declaration);
             }
         }
 
@@ -109,10 +112,140 @@ public class PdlToSmtTranslator extends AbstractTranslator
             FunctionDeclaration declaration = new FunctionDeclaration(name, AbstractTranslator.setOfBinaryAtomSort);
             this.programsMap.put(name, declaration);
             this.smtProgram.addFunction(declaration);
-            for (Transition transition: entry.getValue())
+            if (pdlProgram.isFrameProvided())
             {
-                throw new UnsupportedOperationException();
+                translateProgramTransitions(entry.getValue(), declaration);
             }
+        }
+    }
+
+    private void translateProgramTransitions(List<Transition> transitions, FunctionDeclaration declaration)
+    {
+        if (transitions.size() == 0)
+        {
+            // program is empty
+            Expression emptySet = new UnaryExpression(UnaryExpression.Op.EMPTYSET, AbstractTranslator.setOfBinaryAtomSort);
+            Expression equality = new BinaryExpression(emptySet, BinaryExpression.Op.EQ,
+                    declaration.getVariable());
+            Assertion assertion = new Assertion(declaration.getName() + " is empty ", equality);
+            smtProgram.addAssertion(assertion);
+        }
+        else
+        {
+            List<Expression> sets = transitions.stream()
+                                          .map(t -> getSingletonTransition(t))
+                                          .collect(Collectors.toList());
+            Expression union = sets.get(0);
+            if(sets.size() > 1)
+            {
+                for (int i = 1; i < sets.size(); i++)
+                {
+                    union = new BinaryExpression(union, BinaryExpression.Op.UNION, sets.get(i));
+                }
+            }
+            Expression equality = new BinaryExpression(declaration.getVariable(), BinaryExpression.Op.EQ, union);
+            Assertion assertion = new Assertion("Atomic program " + declaration.getName(), equality);
+            smtProgram.addAssertion(assertion);
+        }
+    }
+
+    private Expression getSingletonTransition(Transition transition)
+    {
+        if (!statesMap.containsKey(transition.inputState))
+        {
+            throw new RuntimeException(String.format("Input state '%s' is not defined in K", transition.inputState));
+        }
+        if (!statesMap.containsKey(transition.outputState))
+        {
+            throw new RuntimeException(String.format("Output state '%s' is not defined in K", transition.outputState));
+        }
+
+        Expression input = statesMap.get(transition.inputState).getVariable();
+        Expression output = statesMap.get(transition.outputState).getVariable();
+        Expression tuple = new MultiArityExpression(MultiArityExpression.Op.MKTUPLE, input, output);
+        Expression singleton = new UnaryExpression(UnaryExpression.Op.SINGLETON, tuple);
+        return singleton;
+    }
+
+    private void translatePropositionStates(List<String> states, FunctionDeclaration declaration)
+    {
+        if (states.size() == 0)
+        {
+            // proposition is empty
+            Expression emptySet = new UnaryExpression(UnaryExpression.Op.EMPTYSET, AbstractTranslator.setOfUnaryAtomSort);
+            Expression equality = new BinaryExpression(emptySet, BinaryExpression.Op.EQ,
+                    declaration.getVariable());
+            Assertion assertion = new Assertion(declaration.getName() + " is empty ", equality);
+            smtProgram.addAssertion(assertion);
+        }
+        else
+        {
+            List<Expression> sets = states.stream()
+                                          .map(s -> getSingletonState(s))
+                                          .collect(Collectors.toList());
+            Expression union = sets.get(0);
+            if(sets.size() > 1)
+            {
+                for (int i = 1; i < sets.size(); i++)
+                {
+                    union = new BinaryExpression(union, BinaryExpression.Op.UNION, sets.get(i));
+                }
+            }
+            Expression equality = new BinaryExpression(declaration.getVariable(), BinaryExpression.Op.EQ, union);
+            Assertion assertion = new Assertion("Atomic proposition " + declaration.getName(), equality);
+            smtProgram.addAssertion(assertion);
+        }
+    }
+
+    private Expression getSingletonState(String state)
+    {
+        if (!statesMap.containsKey(state))
+        {
+            throw new RuntimeException(String.format("State '%s' is not defined in K", state));
+        }
+        Expression variable = statesMap.get(state).getVariable();
+        Expression tuple = new MultiArityExpression(MultiArityExpression.Op.MKTUPLE, variable);
+        Expression singleton = new UnaryExpression(UnaryExpression.Op.SINGLETON, tuple);
+        return singleton;
+    }
+
+    private void translateFrameStates(KripkeFrame frame)
+    {
+        if (frame.getStates().size() > 0)
+        {
+            for (String state : frame.getStates())
+            {
+                FunctionDeclaration stateFunction = new FunctionDeclaration(state, AbstractTranslator.atomSort);
+                statesMap.put(state, stateFunction);
+                smtProgram.addFunction(stateFunction);
+            }
+
+            // states are distinct
+            List<Expression> statesVariables = statesMap.values()
+                                                        .stream().map(s -> s.getVariable())
+                                                        .collect(Collectors.toList());
+            Expression distinct = new MultiArityExpression(MultiArityExpression.Op.DISTINCT, statesVariables);
+            Assertion assertion1 = new Assertion("Frame states are distinct", distinct);
+            smtProgram.addAssertion(assertion1);
+
+            // the universe set should be equal to the union of frame states
+            List<Expression> statesTuples = statesVariables
+                    .stream()
+                    .map(s -> new MultiArityExpression(MultiArityExpression.Op.MKTUPLE, s))
+                    .map(s -> new UnaryExpression(UnaryExpression.Op.SINGLETON, s))
+                    .collect(Collectors.toList());
+            Expression statesSet = statesTuples.get(0);
+            if (statesTuples.size() > 1)
+            {
+                for (int i = 1; i < statesTuples.size(); i++)
+                {
+                    statesSet = new BinaryExpression(statesSet, BinaryExpression.Op.UNION, statesTuples.get(i));
+                }
+
+            }
+            Expression equality = new BinaryExpression(statesUniverse, BinaryExpression.Op.EQ, statesSet);
+            Assertion assertion2 = new Assertion(atomUniverse.getName() + " = frame states", equality);
+            smtProgram.addAssertion(assertion2);
         }
     }
 
--- src/test/java/pdl/translator/PdlToSmtTranslatorTests.java
+++ src/test/java/pdl/translator/PdlToSmtTranslatorTests.java
@@ -388,4 +388,22 @@ class PdlToSmtTranslatorTests
                         "m(q) = {0}\n" +
                         "m(a) = {(0,0)}\n", frame.toString());
     }
+
+    @Test
+    public void bookExample1WithFrame() throws Exception
+    {
+        String pdl =
+                "K = {u, v, w}\n" +
+                "m(p) = {u, v}\n" +
+                "m(a) = {(u,v), (u,w), (v,w), (w,v)}\n" +
+                "<a*>[(a;a)*] p and <a*> [(a;a)*] not p";
+        PdlResult result = PdlUtils.runCVC4(pdl);
+        assertEquals("sat", result.satResult);
+
+        KripkeFrame frame = result.getResultFrame();
+        assertEquals(
+                "K = {u, v, w}\n" +
+                        "m(p) = {u, v}\n" +
+                        "m(a) = {(u,v), (u,w), (v,w), (w,v)}\n", frame.toString());
+    }
 }
